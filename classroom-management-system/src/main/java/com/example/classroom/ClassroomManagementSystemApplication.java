5045b82a112292e400a88d42795759674e11cb30
Huỳnh Minh Quân <minhquanthcs257@gmail.com>
2026-07-13 18:27:07 +0700

    Cấu trúc hệ thống

---
+++ classroom-management-system/src/main/java/com/example/classroom/ClassroomManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.classroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroomManagementSystemApplication.class, args);
+	}
+
+}
