bcd11e29e68aa8214a91b3acdd207566d7ee2f4e
Huỳnh Minh Quân <minhquanthcs257@gmail.com>
2026-07-16 13:08:22 +0700

    #6 Thay đổi cấu hình authentication

--- classroom-management-system/src/main/java/com/example/classroom/dto/account/LoginResponseDTO.java
+++ classroom-management-system/src/main/java/com/example/classroom/dto/account/LoginResponseDTO.java
@@ -1,40 +1,87 @@
 package com.example.classroom.dto.account;
 
+
 public class LoginResponseDTO {
 
-        private String message;
+
+    private String accessToken;
+
 
     private String username;
 
+
     private String role;
+
+
+    private String message;
+
+
 
     public LoginResponseDTO() {
     }
 
-    public LoginResponseDTO(String message, String username, String role) {
-        this.message = message;
+
+
+    public LoginResponseDTO(
+            String accessToken,
+            String username,
+            String role,
+            String message
+    ) {
+
+        this.accessToken = accessToken;
         this.username = username;
         this.role = role;
+        this.message = message;
+
     }
+
+
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+
+
+    public String getUsername() {
+        return username;
+    }
+
+
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+
+    public String getRole() {
+        return role;
+    }
+
+
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+
 
     public String getMessage() {
         return message;
     }
 
+
+
     public void setMessage(String message) {
         this.message = message;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getRole() {
-        return role;
-    }
-    public void setRole(String role) {
-        this.role = role;
     }
 
 }
